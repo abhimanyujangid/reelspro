14e236a4b9bfce12e48e7a2a50646c710ffb719f
abhimanyujangid <abhimanyujangid79@gmail.com>
2025-02-08 00:19:28 +0530

    Add middleware for NextAuth authentication and route protection

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import withAuth from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+   function middleware(){
+    return NextResponse.next();
+   },
+   {
+    callbacks:{
+        authorized: async ({ token, req }) => {
+            const { pathname } = req.nextUrl;
+
+            // Allow access to the auth API routes and the login and register pages
+            if(
+                pathname.startsWith("/api/auth") ||
+                pathname === "/login" ||
+                pathname === "/register"
+            ){
+                return true;
+            }
+
+            // Allow access to the home page and the video API routes
+            if(pathname === "/" || pathname.startsWith("/api/videos")){
+                return true;
+            }
+
+            // Allow access to the video page if the user is authenticated
+            return !!token;
+        }
+    }
+   }
+);
+
+
+export const config =  {
+    matcher: [
+      "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+      "/api/((?!_next/static|_next/image|favicon.ico|public/).*)",
+    ],
+}
